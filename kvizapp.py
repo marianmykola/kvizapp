a647268b6282e88a52f261824f7b9f3aee1d84c5
marianmykola <marianmykola88@gmail.com>
2025-04-27 21:02:26 +0200

    Добавлена приветственная страница для Норики

--- kvizapp.py
+++ kvizapp.py
@@ -6,7 +6,7 @@ BOT_TOKEN = "8070824455:AAG-y_oSvl78o8wq3jKr_VWGsQlBnQ6Bkkc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [KeyboardButton(text="Открыть мини-приложение", web_app=WebAppInfo(url="https://твой-сайт.com"))]
+        [KeyboardButton(text="Открыть мини-приложение", web_app=WebAppInfo(url="https://https://kvizapp.vercel.app"))]
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text("Нажми кнопку ниже:", reply_markup=reply_markup)
