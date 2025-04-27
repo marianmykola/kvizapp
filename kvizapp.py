4269d21f256103a7339cf6d6a07b5f6674eab2d0
marianmykola <marianmykola88@seznam.cz>
2025-04-27 19:53:13 +0200

    Update kvizapp.py

--- kvizapp.py
+++ kvizapp.py
@@ -6,7 +6,7 @@ BOT_TOKEN = "8070824455:AAG-y_oSvl78o8wq3jKr_VWGsQlBnQ6Bkkc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [KeyboardButton(text="Открыть мини-приложение", web_app=WebAppInfo(url="https://kvizapp.vercel.app/"))]
+        [KeyboardButton(text="Открыть мини-приложение", web_app=WebAppInfo(url="https://kvizapp.vercel.app"))]
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text("Нажми кнопку ниже:", reply_markup=reply_markup)
@@ -23,7 +23,7 @@ BOT_TOKEN = "8070824455:AAG-y_oSvl78o8wq3jKr_VWGsQlBnQ6Bkkc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [KeyboardButton(text="Открыть мини-приложение", web_app=WebAppInfo(url="https://kvizapp-git-main-marianmykolas-projects.vercel.app"))]
+        [KeyboardButton(text="Открыть мини-приложение", web_app=WebAppInfo(url="https://kvizapp.vercel.app"))]
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text("Нажми кнопку ниже:", reply_markup=reply_markup)
