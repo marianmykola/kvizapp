08c8b364e122abf8d149512ebc994dc8ccf93874
marianmykola <marianmykola88@seznam.cz>
2025-04-28 06:03:54 +0200

    Update kvizapp.py

--- kvizapp.py
+++ kvizapp.py
@@ -2,7 +2,7 @@
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8070824455:AAG-y_oSvl78o8wq3jKr_VWGsQlBnQ6Bkkc"
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
